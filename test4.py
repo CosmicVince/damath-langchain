8eed76fb73ece6436b14d21d6f4f3db7316af488
CosmicVince <gonvincent.alicando4@gmail.com>
2025-03-23 14:22:33 +0800

    Backup 03/23/2025

--- test4.py
+++ test4.py
@@ -18,47 +18,52 @@ class Piece:
         return hash(self.name)
 
 
-def func1(board_state):
+def func5(board_state):
     valid_moves = {}
     for i, element in enumerate(board_state):
-        if isinstance(element, str) and element == 'X':
+        if element == 'X':
             continue
         elif isinstance(element, list):
             piece = element[0]
             if piece is None:
                 continue
-            if isinstance(piece, Piece) and piece.color == 'r' and piece.is_dama:
-                piece.index = i
-                key, value = func2(piece)
-                valid_moves.update({key: value})
+            if not isinstance(piece, Piece) or piece.color != 'r':
+                continue
+            piece.index = i
+            key, value = func6(piece)
+            valid_moves.update({key: value})
     return valid_moves
 
 
-def func2(piece):
-    damamove = [7, 9, -9, -7]
-    moves = []
-    for mv in damamove:
-        dest_index = piece.index + mv
-        holder = []
-        while 0 <= dest_index < len(board_state) and board_state[dest_index][0] is None:
-            holder.append(dest_index)
-            dest_index += mv
-        if holder:
-            moves.append(tuple(holder))
-    return piece.index, moves
+def func6(piece):
+    capmoves = []
+    dia = [7, 9, -7, -9]
+    src_ind = piece.index
+    for d in dia:
+        temp_ind = src_ind + d
+        if not (0 <= temp_ind < len(board_state)):
+            continue
+        if isinstance(board_state[temp_ind], list):
+            nested_piece = board_state[temp_ind][0]
+            if nested_piece is None or nested_piece.color != 'b':
+                continue
+            dest_ind = temp_ind + d
+            if isinstance(board_state[dest_ind], list) and board_state[dest_ind][0] is None:
+                capmoves.append(dest_ind)
+    return piece.index, capmoves
 
 
-# Example usage:
+# Example usage
 board_state = [
-        [None, '*'], 'X', [None, '/'], 'X', [None, '-'], 'X', [None, '+'], 'X', 'X',
-        [None, '/'], 'X', [None, '*'], 'X', [Piece('r', -11, is_dama=False), '+'], 'X',
-        [Piece('r', 0, is_dama=False), '-'], [None, '-'], 'X', [None, '+'], 'X', 
-        [Piece('r', 6, is_dama=True), '*'], 'X', [Piece('r', -9, is_dama=False), '/'], 'X', 'X',
-        [None, '+'], 'X', [None, '-'], 'X', [None, '/'], 'X', [None, '*'], [Piece('b', 0, is_dama=True), '*'], 'X',
+        [Piece('r', -112, is_dama=False), '*'], 'X', [None, '/'], 'X', [None, '-'], 'X', [None, '+'], 'X', 'X',
+        [Piece('b', 0, is_dama=False), '/'], 'X', [None, '*'], 'X', [Piece('b', -11, is_dama=False), '+'], 'X',
+        [None, '-'], [None, '-'], 'X', [None, '+'], 'X', 
+        [None, '*'], 'X', [Piece('r', -9, is_dama=False), '/'], 'X', 'X',
+        [None, '+'], 'X', [None, '-'], 'X', [None, '/'], 'X', [None, '*'], [Piece('b', 0, is_dama=False), '*'], 'X',
         [None, '/'], 'X', [None, '-'], 'X', [None, '+'], 'X', 'X', [None, '/'], 'X',
-        [None, '*'], 'X', [None, '+'], 'X', [None, '-'], [Piece('r', -5, is_dama=True), '-'], 'X',
-        [None, '+'], 'X', [None, '*'], 'X', [None, '/'], 'X', 'X', [None, '+'], 'X',
-        [None, '-'], 'X', [None, '/'], 'X', [None, '*']
+        [None, '*'], 'X', [None, '+'], 'X', [None, '-'], [Piece('r', -5, is_dama=False), '-'], 'X',
+        [None, '+'], 'X', [None, '*'], 'X', [Piece('b', 6, is_dama=False), '/'], 'X', 'X', [None, '+'], 'X',
+        [None, '-'], 'X', [None, '/'], 'X', [Piece('r', 6, is_dama=False), '*']
         ]
 
-print(func1(board_state))
+print(func5(board_state))
